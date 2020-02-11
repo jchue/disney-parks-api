138097a0bca838cabc6673ad5a06f10dd00ba88a
Jonathan Chue <5141427+jchue@users.noreply.github.com>
2020-02-10 21:44:51 -0800

    Update dependencies

--- src/models/index.js
+++ src/models/index.js
@@ -1,6 +1,9 @@
 import mongoose from 'mongoose';
 import event from './event';
 
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+
 const connectDb = () => mongoose.connect(process.env.DBURL);
 const models = { event };
 
